bfd4022c4ec0e19b181c18c296a692d49797eae0
arunkuimarsivasamy <arunkumar.s@prodapt.com>
2019-05-22 18:29:40 +0530

    mypython1 modified

--- mypython1.py
+++ mypython1.py
@@ -1,1 +1,7 @@
-print("hello")
+counter = 100          # An integer assignment
+miles   = 1000.0       # A floating point
+name    = "John"       # A string
+
+print(counter)
+print(miles)
+print(name)
